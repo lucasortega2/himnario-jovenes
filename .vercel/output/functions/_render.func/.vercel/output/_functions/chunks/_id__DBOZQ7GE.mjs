664b5d29f3eeb6e70767b052383ed1e587cec6f0
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-05-13 22:18:29 -0300

    agregado de cancion prueba

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_id__DBOZQ7GE.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_id__4r7mL9P7.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_BSGa7IOp.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_CWJda0xi.mjs').then(n => n.i);
-
-export { page };
